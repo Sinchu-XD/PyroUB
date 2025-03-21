a5f03d651af6be8abcc24313795f6cc4bc1a63c3
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-03-21 19:53:12 +0530

    Update __init__.py

--- Abhi/__init__.py
+++ Abhi/__init__.py
@@ -1,2 +1,15 @@
-from Abhi import app
+import logging
+from pyrogram import Client
 from .Config import API_ID, API_HASH, SESSION_STRING
+
+# Enable logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("Abhi")
+
+# Initialize Pyrofork Client
+app = Client(
+    SESSION_STRING,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins=dict(root="Plugins"),  # Load plugins from the "plugins" directory
+)
