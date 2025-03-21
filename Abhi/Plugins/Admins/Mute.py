6d0f9875de1c19073260cb6e7c40edd579ba1eee
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-03-21 18:13:59 +0530

    Update Mute.py

--- Abhi/Plugins/Admins/Mute.py
+++ Abhi/Plugins/Admins/Mute.py
@@ -5,14 +5,14 @@ from Abhi import app
 PREFIXES = [".", "!"] 
 
 
-@bot.on_message(filters.command("mute", PREFIXES) & filters.group)
+@app.on_message(filters.command("mute", PREFIXES) & filters.group)
 async def mute_user(_, message: Message):
     if not message.reply_to_message:
         return await message.reply("Reply to a user to mute them!")
     await message.chat.restrict_member(message.reply_to_message.from_user.id, can_send_messages=False)
     await message.reply("User has been muted! 🤐")
 
-@bot.on_message(filters.command("unmute", PREFIXES) & filters.group)
+@app.on_message(filters.command("unmute", PREFIXES) & filters.group)
 async def unmute_user(_, message: Message):
     if not message.reply_to_message:
         return await message.reply("Reply to a user to unmute them!")
