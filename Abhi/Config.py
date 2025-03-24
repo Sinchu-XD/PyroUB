6ac0c5e16cb9874e971f9a423124c11b0b3a598f
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-03-24 17:50:48 +0530

    Update Config.py

--- Abhi/Config.py
+++ Abhi/Config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 API_ID = int(os.getenv("API_ID", 25024171))
 API_HASH = os.getenv("API_HASH", "7e709c0f5a2b8ed7d5f90a48219cffd3")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7726535663:AAEZj1J9JzNEJFMB1hHOMIf-wkgDyVc_y8I")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7726535663:AAGWWYg6mOUF3MvsFH1bbEqo43X8JteFm9o")
